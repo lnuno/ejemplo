2df24264a77d0b6ff76a62f0c31b26774633f66a
lnuno <100434469+lnuno@users.noreply.github.com>
2022-06-17 11:43:08 -0500

    cambio

---
+++ 0-Pruebas.R
@@ -0,0 +1,3 @@
+#cambiar directorio de trabajo
+
+setwd(r"{C:\Users\Rvilla\OneDrive - LABORATORIOS PISA S.A. DE C.V\Ciencia De Datos - INTELIGENCIA COMERCIAL DEMANDA MEDICA\Proyectos\10 MFP (Marcas Propias)}")
